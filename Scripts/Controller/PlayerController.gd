be70d874baa53fecba2c0916f24cfb0fa40e30a3
Afonso Lage <lage.afonso@gmail.com>
2020-08-02 10:10:47 -0300

    Added attack animation on main player

--- Scripts/Controller/PlayerController.gd
+++ Scripts/Controller/PlayerController.gd
@@ -5,25 +5,57 @@ export(int) var speed_plus = 200
 
 onready var animation_tree = $AnimationTree
 onready var animation_state = animation_tree.get("parameters/playback")
+onready var state = State.IDLING
 
-func get_input():
+var input_direction = Vector2.ZERO
+var input_action = false
+
+enum State {
+	IDLING,
+	WALKING,
+	ATTACKING,
+}
+
+func process_input():
 	var velocity = Vector2.ZERO
 	velocity.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	velocity.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	velocity = velocity.normalized() * (speed_plus if Input.is_key_pressed(KEY_CONTROL) else speed)
-	return velocity
+	input_direction = velocity
+	
+	input_action = Input.is_action_just_pressed("ui_action")
 	
 func _physics_process(delta):
-	var input_direction = get_input()
+	process_input()
 	
-	if (input_direction != Vector2.ZERO):
-		animation_tree.set("parameters/idle/blend_position", input_direction)
-		animation_tree.set("parameters/walk/blend_position", input_direction)
-		animation_state.travel("walk")
-		var new_velocity = move_and_slide(input_direction)
-	else:
-		animation_state.travel("idle")
-		animation_tree.set("parameters/idle/blend_position", input_direction)
+	if state == State.ATTACKING:
+		return
 	
-#	if (new_velocity != velocity):
-#		emit_signal("moving", velocity)
+	if input_action:
+		set_attacking()
+	elif input_direction != Vector2.ZERO:
+		set_walking()
+	elif state != State.IDLING:
+		set_idle()
+
+func set_idle():
+	state = State.IDLING
+	animation_state.travel("idle")
+	
+func set_walking():
+	state = State.WALKING
+	animation_tree.set("parameters/idle/blend_position", input_direction)
+	animation_tree.set("parameters/walk/blend_position", input_direction)
+	animation_tree.set("parameters/attack/blend_position", input_direction)
+	animation_state.travel("walk")
+	var new_velocity = move_and_slide(input_direction)
+	
+	if new_velocity.length() < 0.1:
+		set_idle()
+
+func set_attacking():
+	state = State.ATTACKING
+	animation_state.travel("attack")
+	
+func attack_finished():
+	set_idle()
